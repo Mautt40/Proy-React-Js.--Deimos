4545be7873c83cf72affacb8a44abead62e19fab
Mautt40 <tondato.t100@gmail.com>
2023-03-27 12:24:01 -0300

    primeros archivos

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+  let nombre = "Mauricio";
+
+  return (
+    <div>
+      <h1> HOLA DESDE EL COMPONENTE LOGIN</h1>
+      <h3> mi nombre es {nombre}</h3>
+    </div>
+  );
+};

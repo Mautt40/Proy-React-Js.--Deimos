b0916be28e7c9fa65db584cc43265f39adcbb6ec
Mautt40 <tondato.t100@gmail.com>
2023-04-14 11:12:38 -0300

    modificado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
-import FetchingData from "./components/FetchingData/FetchingData";
-
 import ItemListContainer from "./components/ItemList/ItemListContainer";
 import {Navbar} from "./components/Navbar/Navbar"
 
 function App() {
   
- let saludo = "DEIMOS - Librería especializada -";  
+ 
   
  return (
     <div className="App">
       <Navbar />
       <ItemListContainer/>
-      
     
     </div>
   )

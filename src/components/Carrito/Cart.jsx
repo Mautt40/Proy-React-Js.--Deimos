a303cc561b15ec6ad7c9379fcb1b0399db24c7b1
Mautt40 <tondato.t100@gmail.com>
2023-05-02 14:40:53 -0300

    estilos al cart

--- src/components/Carrito/Cart.jsx
+++ src/components/Carrito/Cart.jsx
@@ -2,12 +2,17 @@ import { Button } from "@mui/material";
 import React from "react";
 import "./Cart.css"
 import { Link } from "react-router-dom";
+import Logo from "../../assets/img/Logo.png";
+import { BsCart4 } from "react-icons/bs";
 
 const Cart = ({ cart, clearCart, deleteProductById, total }) => {
   return (
     <div>
       <div className="cart-container">
+        
         <div className="container-items">
+       <h1>  <BsCart4 size={40} style={{fill: "white" }} />    Agregados al carrito</h1>
+          
           {cart.map((item) => {
             return (
               <div className="cart-item" key={item.id}>
@@ -36,10 +41,10 @@ const Cart = ({ cart, clearCart, deleteProductById, total }) => {
         
         
         <div className="cart-info">
-          <h3>Cantidad de productos: </h3>
-          <h3>Precio total: ${total},00</h3>
+          <img src={Logo} alt="logo deimos" /> 
+          <h3>Precio total: $ {total},00</h3>
           <h3>Descuento: </h3>
-          <h3>Precio final: ${total},00 </h3>
+          <h3>Precio final: $ {total},00 </h3>
           {cart.length > 0 ? (
             <div className="btn-cart">
               <Button variant="contained" color="warning">Terminar la compra</Button>
@@ -49,11 +54,11 @@ const Cart = ({ cart, clearCart, deleteProductById, total }) => {
             </div>
           ) : (
             <Link to="/">
-              <Button variant="contained">Agrega productos</Button>
+              <Button variant="contained" color="warning">Agrega productos</Button>
             </Link>
           )}
 
-          <h1>El total del carrito es ${total},00</h1>
+          <h6>El total de su compra es $ {total},00</h6>
         </div>
       </div>
     </div>
